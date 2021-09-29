c85f2d01a433938775a0366f91b1e12ccac42fcc
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-29 07:19:34 +0200

    refactor: export json instead of class

--- src/BuildInformation.ts
+++ src/BuildInformation.ts
@@ -1,26 +1,14 @@
-import { ServalBuildInformation } from "@js-soft/ts-serval"
-import { CryptoBuildInformation } from "@nmshd/crypto"
-import { TransportBuildInformation } from "@nmshd/transport"
+import { buildInformation as servalBuildInformation } from "@js-soft/ts-serval"
+import { buildInformation as cryptoBuildInformation } from "@nmshd/crypto"
+import { buildInformation as transportBuildInformation } from "@nmshd/transport"
 
-export class ContentBuildInformation {
-    public readonly version: string = "{{version}}"
-    public readonly build: string = "{{build}}"
-    public readonly date: string = "{{date}}"
-    public readonly commit: string = "{{commit}}"
-    public readonly dependencies: object
-
-    public readonly transport = TransportBuildInformation.info
-    public readonly crypto = CryptoBuildInformation.info
-    public readonly serval = ServalBuildInformation.info
-
-    private constructor() {
-        try {
-            // eslint-disable-next-line @typescript-eslint/quotes
-            this.dependencies = JSON.parse(`{{dependencies}}`)
-        } catch (e) {
-            this.dependencies = {}
-        }
-    }
-
-    public static readonly info: ContentBuildInformation = new ContentBuildInformation()
+export const buildInformation = {
+    version: "{{version}}",
+    build: "{{build}}",
+    date: "{{date}}",
+    commit: "{{commit}}",
+    dependencies: "{{dependencies}}",
+    serval: servalBuildInformation,
+    crypto: cryptoBuildInformation,
+    transport: transportBuildInformation
 }
