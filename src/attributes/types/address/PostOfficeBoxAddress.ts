cb7498d5d22072f0d92fdb155916f86db6dbe9d3
lzimmerm <lea.zimmermann@js-soft.com>
2022-03-10 09:47:17 +0100

    refactor: adapt code style

--- src/attributes/types/address/PostOfficeBoxAddress.ts
+++ src/attributes/types/address/PostOfficeBoxAddress.ts
@@ -1,29 +1,29 @@
-import { serialize, type, validate } from "@js-soft/ts-serval";
-import { Address } from "./Address";
-import { City } from "./City";
-import { Country } from "./Country";
-import { State } from "./State";
-import { ZipCode } from "./ZipCode";
+import { serialize, type, validate } from "@js-soft/ts-serval"
+import { Address } from "./Address"
+import { City } from "./City"
+import { Country } from "./Country"
+import { State } from "./State"
+import { ZipCode } from "./ZipCode"
 
 @type("PostOfficeBoxAddress")
 export class PostOfficeBoxAddress extends Address {
     @serialize()
     @validate()
-    public boxId: string;
+    public boxId: string
 
     @serialize()
     @validate()
-    public zipCode: ZipCode;
+    public zipCode: ZipCode
 
     @serialize()
     @validate()
-    public city: City;
+    public city: City
 
     @serialize()
     @validate()
-    public country: Country;
+    public country: Country
 
     @serialize()
     @validate()
-    public state: State;
+    public state: State
 }
