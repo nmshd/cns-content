02e4a7a557a803cd723e1cba22bf2c385bab492c
tnotheis <timo.notheisen@js-soft.com>
2022-03-15 08:47:55 +0100

    chore: formatting

--- src/attributes/types/AbstractMonth.ts
+++ src/attributes/types/AbstractMonth.ts
@@ -1,22 +1,22 @@
 import { serialize, validate } from "@js-soft/ts-serval"
 import { AbstractAttributeValue } from "../AbstractAttributeValue"
-/**
- * Month value are continuously numbered: 1 (january) - 12 (december)
- */
 
+/**
+ * Month values: 1 (january) - 12 (december)
+ */
 enum Month {
     January = 1,
-    February,
-    March,
-    April,
-    May,
-    June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
 }
 
 export abstract class AbstractMonth extends AbstractAttributeValue {
