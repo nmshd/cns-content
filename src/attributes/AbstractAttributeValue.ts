106378d64e5a596de63d4aa4967ad5f23339df01
tnotheis <timo.notheisen@js-soft.com>
2022-03-15 12:11:00 +0100

    fix: remove type from AbstractAttributeValue

--- src/attributes/AbstractAttributeValue.ts
+++ src/attributes/AbstractAttributeValue.ts
@@ -1,11 +1,10 @@
-import { ISerializable, Serializable, type } from "@js-soft/ts-serval"
+import { ISerializable, Serializable } from "@js-soft/ts-serval"
 import { ContentJSON } from "../ContentJSON"
 
 export interface AbstractAttributeValueJSON extends ContentJSON, Record<string, unknown> {}
 
 export interface IAbstractAttributeValue extends ISerializable, Record<string, unknown> {}
 
-@type("AbstractAttribute")
 export abstract class AbstractAttributeValue extends Serializable implements IAbstractAttributeValue {
     [key: string]: unknown
 }
