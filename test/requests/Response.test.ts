efbffa9d47a8510c8341e7c2fc7467112be1df96
tnotheis <timo.notheisen@js-soft.com>
2022-03-16 09:19:28 +0100

    Revert "chore: remove redundant await/async"
    This reverts commit 5d2d824aee6c41fe169fe8235d5e610a9116584e.

--- test/requests/Response.test.ts
+++ test/requests/Response.test.ts
@@ -20,7 +20,7 @@ import { expectThrowsAsync } from "../testUtils"
 export class ResponseTest extends AbstractTest {
     public run(): void {
         describe("Response", function () {
-            it("creates a Response and items from JSON", function () {
+            it("creates a Response and items from JSON", async function () {
                 const responseJSON = {
                     "@type": "Response",
                     requestId: "CNSREQ1",
@@ -41,7 +41,7 @@ export class ResponseTest extends AbstractTest {
                     ]
                 } as ResponseJSON
 
-                const response = Response.from(responseJSON)
+                const response = await Response.from(responseJSON)
 
                 expect(response).to.be.instanceOf(Response)
                 expect(response.items).to.have.lengthOf(2)
@@ -75,7 +75,7 @@ export class ResponseTest extends AbstractTest {
                     ]
                 } as IResponse
 
-                const response = Response.from(responseInterface)
+                const response = await Response.from(responseInterface)
 
                 expect(response).to.be.instanceOf(Response)
                 expect(response.items).to.have.lengthOf(2)
@@ -88,7 +88,7 @@ export class ResponseTest extends AbstractTest {
                 expect(responseItemGroup.items).to.have.lengthOf(1)
             })
 
-            it("keeps all properties during serialization and deserialization", function () {
+            it("keeps all properties during serialization and deserialization", async function () {
                 const responseJSON = {
                     "@type": "Response",
                     requestId: "CNSREQ1",
@@ -128,7 +128,7 @@ export class ResponseTest extends AbstractTest {
                     ]
                 } as ResponseJSON
 
-                const response = Response.from(responseJSON)
+                const response = await Response.from(responseJSON)
 
                 const serializedRequest = response.toJSON()
 
