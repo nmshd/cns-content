05eb243255a782cbf9be932ada55de6c0516db08
tnotheis <timo.notheisen@js-soft.com>
2022-03-17 13:57:51 +0100

    chore: remove deprecated key property from Request

--- src/requests/Request.ts
+++ src/requests/Request.ts
@@ -8,15 +8,6 @@ export interface RequestJSON extends ContentJSON {
     id?: string
 
     /**
-     * The technial key of the Request which is submitted back with the answer. This can be used
-     * for mapping requests and answers. The key should be unique and non-personal, best a random UUID.
-     * @default undefined - no key is used
-     * @deprecated `key` should not be used anymore. Instead, use {@link RequestItemJSON.responseMetadata RequestItem.responseMetadata}
-     * or {@link RequestItemGroupJSON.responseMetadata RequestItemGroup.responseMetadata}.
-     */
-    key?: string
-
-    /**
      * The point in time the request is considered obsolete either technically (e.g. the request is no longer
      * valid or its response is no longer accepted) or from a business perspective (e.g. the request is no longer
      * of interest).
@@ -33,15 +24,6 @@ export interface RequestJSON extends ContentJSON {
 
 export interface IRequest extends ISerializableAsync {
     id?: CoreId
-
-    /**
-     * The technial key of the Request which is submitted back with the answer. This can be used
-     * for mapping requests and answers. The key should be unique and non-personal, best a random UUID.
-     * @default undefined - no key is used
-     * @deprecated `key` should not be used anymore. Instead, use {@link RequestItem.responseMetadata RequestItem.responseMetadata}
-     * or {@link RequestItemGroup.responseMetadata RequestItemGroup.responseMetadata}.
-     */
-    key?: string
 
     /**
      * The point in time the request is considered obsolete either technically (e.g. the request is no longer
