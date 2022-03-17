069945a8f801a633bce97c200b020d20aa60a427
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2022-03-17 13:32:54 +0100

    Feature/jssnmshdd 2919 delete unused request types (#16)
    * feat: delete unused request types
    
    * chore: delete unused Relationship types
    
    * chore: use remaining Request types directly in RequestMail
    
    * chore: update index.ts
    
    * test: remove instanceof Request checks from tests
    
    * chore: set version to 1.2.1

--- src/requests/AttributesChangeRequest.ts
+++ src/requests/AttributesChangeRequest.ts
@@ -1,20 +1,73 @@
-import { serialize, type, validate } from "@js-soft/ts-serval"
-import { CoreAddress, CoreDate, CoreId, ICoreAddress } from "@nmshd/transport"
+import { ISerializable, SerializableAsync, serialize, type, validate } from "@js-soft/ts-serval"
+import { CoreAddress, CoreDate, CoreId, ICoreAddress, ICoreDate, ICoreId } from "@nmshd/transport"
 import { Attribute, AttributeJSON, IAttribute } from "../attributes/Attribute"
-import { IRequest, Request, RequestJSON } from "./Request"
+import { ContentJSON } from "../ContentJSON"
 
-export interface AttributesChangeRequestJSON extends RequestJSON {
+export interface AttributesChangeRequestJSON extends ContentJSON {
+    id?: string
+    key?: string
+    reason?: string
+    expiresAt?: string
+    impact?: string
     attributes: AttributeJSON[]
     applyTo?: string
 }
 
-export interface IAttributesChangeRequest extends IRequest {
+export interface IAttributesChangeRequest extends ISerializable {
+    id?: ICoreId
+    /**
+     * The technial key of the request which is submitted back with the answer. This can be used
+     * for mapping requests and answers. The key should be unique and non-personal, best a random UUID.
+     * @default undefined - no key is used
+     */
+    key?: string
+    /**
+     * The human-readable reason for this request. This can be used to provide the user with more input
+     * why this request is necessary and should be answered.
+     * @default undefined - no reason is given
+     */
+    reason?: string
+    /**
+     * The point in time the request is considered obsolete either technically (e.g. the request is no longer
+     * valid or its response is no longer accepted) or from a business perspective (e.g. the request is no longer
+     * of interest).
+     * @default undefined - the request won't expire
+     */
+    expiresAt?: ICoreDate
+
+    /**
+     * The impact of this request. This can be used to show the user what happens if the user does not accept the request.
+     *
+     * @default undefined - no specific impact
+     */
+    impact?: string
+
     attributes: IAttribute[]
     applyTo?: ICoreAddress
 }
 
 @type("AttributesChangeRequest")
-export class AttributesChangeRequest extends Request implements IAttributesChangeRequest {
+export class AttributesChangeRequest extends SerializableAsync implements IAttributesChangeRequest {
+    @serialize()
+    @validate({ nullable: true })
+    public id?: CoreId
+
+    @serialize()
+    @validate({ nullable: true })
+    public key?: string
+
+    @serialize()
+    @validate({ nullable: true })
+    public reason?: string
+
+    @serialize()
+    @validate({ nullable: true })
+    public expiresAt?: CoreDate
+
+    @serialize()
+    @validate({ nullable: true })
+    public impact?: string
+
     @serialize({ type: Attribute })
     @validate()
     public attributes: Attribute[]
