be09286cf8b0d5c773d5df6305b9dffdf11b3b41
Lea Zimmermann <35802341+Lea-Zim@users.noreply.github.com>
2022-03-17 17:24:39 +0100

    Feature/jssnmshdd 2918 define attribute query (#17)
    * chore: remove unused files
    
    * feat: add attributeQuery interface
    
    * chore: update content version
    
    * feat: add tests for AttributeQuery
    
    * fix: fix paths in AttributeQuery tests
    
    * fix: remove attributeQueryContainer
    
    * fix: path in AttributesRequest
    
    * chore: increase version
    
    * fix: remove unnecessary properties
    
    Co-authored-by: lzimmerm <lea.zimmermann@js-soft.com>

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import { SimpleLoggerFactory } from "@js-soft/simple-logger"
 import { AttributeTest } from "./attributes/Attribute.test"
+import { AttributeQueryTest } from "./attributes/AttributeQuery.test"
 import { MailTest } from "./messages/Mail.test"
 import { RequestMailTest } from "./messages/RequestMail.test"
 
@@ -8,3 +9,4 @@ const loggerFactory = new SimpleLoggerFactory()
 new MailTest(loggerFactory).run()
 new RequestMailTest(loggerFactory).run()
 new AttributeTest(loggerFactory).run()
+new AttributeQueryTest(loggerFactory).run()
