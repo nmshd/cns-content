7cc8574362649972433bb818ca750443cd85ef4a
jkoenig <julian.koenig@js-soft.com>
2022-03-21 14:15:00 +0100

    chore: update workflows as in release/2.0.0

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -1,5 +1,18 @@
 set -e
 set -x
 
+if [ -z "$(which jq)" ]; then
+    echo "jq could not be found"
+    exit 1
+fi
+
+VERSION=$(jq .version package.json -cr)
+
 npm set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
-npm publish
+
+case "$VERSION" in
+*-alpha*) npm publish --tag alpha ;;
+*-beta*) npm publish --tag beta ;;
+*-rc*) npm publish --tag next ;;
+*) npm publish ;;
+esac
