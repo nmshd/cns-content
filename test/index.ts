973488dadb137b765a036d05d01dad5aea4292ea
tnotheis <timo.notheisen@js-soft.com>
2022-03-21 13:41:11 +0100

    feat: add AttributeQuery

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import { SimpleLoggerFactory } from "@js-soft/simple-logger"
 import { AttributeTest } from "./attributes/Attribute.test"
+import { AttributeQueryTest } from "./attributes/AttributeQuery.test"
 import { MailTest } from "./messages/Mail.test"
 import { RequestMailTest } from "./messages/RequestMail.test"
 import { RequestTest } from "./requests/Request.test"
@@ -12,3 +13,4 @@ new RequestTest(loggerFactory).run()
 new ResponseTest(loggerFactory).run()
 new RequestMailTest(loggerFactory).run()
 new AttributeTest(loggerFactory).run()
+new AttributeQueryTest(loggerFactory).run()
