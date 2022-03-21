5ef807fc5a11d72a894fbccd2670d88e6d60be0e
jkoenig <julian.koenig@js-soft.com>
2022-03-21 14:15:34 +0100

    ci: use enhanced-publish to publish the packages

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -11,8 +11,8 @@ VERSION=$(jq .version package.json -cr)
 npm set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
 
 case "$VERSION" in
-*-alpha*) npm publish --tag alpha ;;
-*-beta*) npm publish --tag beta ;;
-*-rc*) npm publish --tag next ;;
-*) npm publish ;;
+*-alpha*) npx enhanced-publish --if-possible --tag alpha ;;
+*-beta*) npx enhanced-publish --if-possible --tag beta ;;
+*-rc*) npx enhanced-publish --if-possible --tag next ;;
+*) npx enhanced-publish --if-possible ;;
 esac
