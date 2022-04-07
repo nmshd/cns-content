f2e574f2d273c3db0fe327ea0e149c9504104cc8
lzimmerm <lea.zimmermann@js-soft.com>
2022-04-07 15:41:44 +0200

    fix: validation of date format

--- src/attributes/types/AbstractDateString.ts
+++ src/attributes/types/AbstractDateString.ts
@@ -1,12 +1,12 @@
 import { serialize, validate } from "@js-soft/ts-serval"
+import { DateTime } from "luxon"
 import { AbstractAttributeValue } from "../AbstractAttributeValue"
-
-const dateRegex = new RegExp("d{4}-1[0-2]|0[1-9]-3[01]|[12][0-9]|0[1-9]")
 
 export abstract class AbstractDateString extends AbstractAttributeValue {
     @serialize()
     @validate({
-        customValidator: (v) => (!dateRegex.test(v) ? "must match the following format: 'yyyy-MM-dd'" : undefined)
+        customValidator: (v) =>
+            !DateTime.fromFormat(v, "yyyy-MM-dd").isValid ? "must match the following format: 'yyyy-MM-dd'" : undefined
     })
     public value: string
 }
