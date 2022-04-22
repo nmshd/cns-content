062bf03e1410f628f5015590d81412360a834b94
Stnmtz <t.stnmtz@outlook.de>
2022-04-22 17:16:29 +0200

    feat: add communication types

---
+++ src/attributes/types/communication/CommunicationLanguage.ts
@@ -0,0 +1,5 @@
+import { type } from "@js-soft/ts-serval"
+import { AbstractLanguageValue } from "../strings/AbstractLanguageValue"
+
+@type("CommunicationLanguage")
+export class CommunicationLanguage extends AbstractLanguageValue {}
---
+++ src/attributes/types/communication/EMailAddress.ts
@@ -0,0 +1,5 @@
+import { type } from "@js-soft/ts-serval"
+import { AbstractEMailAddressValue } from "../strings/AbstractEMailAddressValue"
+
+@type("EMailAddress")
+export class EMailAddress extends AbstractEMailAddressValue {}
