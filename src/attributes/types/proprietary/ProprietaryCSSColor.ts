100e7debb3392c7c12c9e492bde7881febc457c7
Stnmtz <t.stnmtz@outlook.de>
2022-04-26 08:45:17 +0200

    fix: only HEXColor

--- src/attributes/types/proprietary/ProprietaryCSSColor.ts
+++
@@ -1,5 +0,0 @@
-import { type } from "@js-soft/ts-serval"
-import { AbstractCSSColorValue } from "../strings/AbstractCSSColorValue"
-
-@type("ProprietaryCSSColor")
-export class ProprietaryCSSColor extends AbstractCSSColorValue {}
---
+++ src/attributes/types/proprietary/ProprietaryHEXColor.ts
@@ -0,0 +1,5 @@
+import { type } from "@js-soft/ts-serval"
+import { AbstractHEXColorValue } from "../strings/AbstractHEXColorValue"
+
+@type("ProprietaryHEXColor")
+export class ProprietaryHEXColor extends AbstractHEXColorValue {}
