b5eb3126d1d0ec4b5996447cead2110ae07f62f6
Stnmtz <t.stnmtz@outlook.de>
2022-04-28 21:52:17 +0200

    fix: no need for nationality as it is the country

--- src/attributes/types/strings/AbstractNationality.ts
+++
@@ -1,8 +0,0 @@
-import { serialize, validate } from "@js-soft/ts-serval"
-import { AbstractString } from "../AbstractString"
-
-export abstract class AbstractNationality extends AbstractString {
-    @serialize()
-    @validate()
-    public override value: string
-}
