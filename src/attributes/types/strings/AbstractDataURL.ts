113a516ee9ed7ced97807d833bfee9085b56ec7c
Stnmtz <t.stnmtz@outlook.de>
2022-04-29 16:07:21 +0200

    refactor: remove override

--- src/attributes/types/strings/AbstractDataURL.ts
+++ src/attributes/types/strings/AbstractDataURL.ts
@@ -1,8 +1,3 @@
-import { serialize, validate } from "@js-soft/ts-serval"
 import { AbstractString } from "../AbstractString"
 
-export abstract class AbstractDataURL extends AbstractString {
-    @serialize()
-    @validate()
-    public override value: string
-}
+export abstract class AbstractDataURL extends AbstractString {}
