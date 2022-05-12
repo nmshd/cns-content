ef96b4f6fdb32b4ca1ddb6e74e16d2efc4d581e8
Stnmtz <t.stnmtz@outlook.de>
2022-05-12 18:15:01 +0200

    feat: add hints to query

--- src/attributes/RelationshipAttributeQuery.ts
+++ src/attributes/RelationshipAttributeQuery.ts
@@ -1,15 +1,20 @@
 import { serialize, type, validate } from "@js-soft/ts-serval"
 import { CoreAddress, ICoreAddress } from "@nmshd/transport"
 import { AbstractAttributeQuery, AbstractAttributeQueryJSON, IAbstractAttributeQuery } from "./AbstractAttributeQuery"
+import { AttributeHints, AttributeHintsJSON, IAttributeHints } from "./hints/AttributeHints"
 
 export interface RelationshipAttributeQueryJSON extends AbstractAttributeQueryJSON {
     key: string
     owner: string
+    attributeHints: AttributeHintsJSON
+    thirdParty?: string
 }
 
 export interface IRelationshipAttributeQuery extends IAbstractAttributeQuery {
     key: string
     owner: ICoreAddress
+    attributeHints: IAttributeHints
+    thirdParty?: ICoreAddress
 }
 
 @type("RelationshipAttributeQuery")
@@ -21,6 +26,14 @@ export class RelationshipAttributeQuery extends AbstractAttributeQuery implement
     @serialize()
     @validate()
     public owner: CoreAddress
+
+    @serialize()
+    @validate()
+    public attributeHints: AttributeHints
+
+    @serialize()
+    @validate({ nullable: true })
+    public thirdParty?: CoreAddress
 
     public static from(
         value: IRelationshipAttributeQuery | RelationshipAttributeQueryJSON
