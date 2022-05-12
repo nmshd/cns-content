a3919aac211a61ed52f9e193849a1b1b08164ba1
Stnmtz <t.stnmtz@outlook.de>
2022-05-12 18:14:46 +0200

    feat: add hints

---
+++ src/attributes/hints/AttributeHints.ts
@@ -0,0 +1,38 @@
+import { ISerializable, Serializable, serialize, type, validate } from "@js-soft/ts-serval"
+import { IValueHints, ValueHints, ValueHintsJSON } from "./ValueHints"
+
+export interface AttributeHintsJSON {
+    title: string
+    description?: string
+    valueHints?: ValueHintsJSON
+}
+
+export interface IAttributeHints extends ISerializable {
+    title: string
+    description?: string
+    valueHints?: IValueHints
+}
+
+/**
+ * AttributeHints are rendering hints with a `title` and a possible `description` set.
+ * They are primarily used within `RelationshipAttributeQuery` to define the metadata of
+ * a proprietary Attribute, even without such an Attribute existant.
+ */
+@type("AttributeHints")
+export class AttributeHints extends Serializable implements IAttributeHints {
+    @serialize()
+    @validate()
+    public title: string
+
+    @serialize()
+    @validate({ nullable: true })
+    public description?: string
+
+    @serialize()
+    @validate({ nullable: true })
+    public valueHints?: ValueHints
+
+    public static from(value: IAttributeHints | AttributeHintsJSON): AttributeHints {
+        return this.fromAny(value)
+    }
+}
