3ed5c18a6908a17f3b685e984863b1c854454e16
Stnmtz <t.stnmtz@outlook.de>
2022-05-16 20:58:34 +0200

    feat: add properties to hints

--- src/attributes/hints/RelationshipAttributeHints.ts
+++ src/attributes/hints/RelationshipAttributeHints.ts
@@ -1,16 +1,21 @@
 import { ISerializable, Serializable, serialize, type, validate } from "@js-soft/ts-serval"
+import { RelationshipAttributeConfidentiality } from "../RelationshipAttribute"
 import { IValueHints, ValueHints, ValueHintsJSON } from "./ValueHints"
 
 export interface RelationshipAttributeHintsJSON {
     title: string
     description?: string
     valueHints?: ValueHintsJSON
+    isTechnical?: boolean
+    confidentiality: RelationshipAttributeConfidentiality
 }
 
 export interface IRelationshipAttributeHints extends ISerializable {
     title: string
     description?: string
     valueHints?: IValueHints
+    isTechnical?: boolean
+    confidentiality: RelationshipAttributeConfidentiality
 }
 
 /**
@@ -31,6 +36,18 @@ export class RelationshipAttributeHints extends Serializable implements IRelatio
     @serialize()
     @validate({ nullable: true })
     public valueHints?: ValueHints
+
+    @serialize()
+    @validate()
+    public isTechnical = false
+    @serialize()
+    @validate({
+        customValidator: (v) =>
+            !Object.values(RelationshipAttributeConfidentiality).includes(v)
+                ? `must be one of: ${Object.values(RelationshipAttributeConfidentiality)}`
+                : undefined
+    })
+    public confidentiality: RelationshipAttributeConfidentiality
 
     public static from(
         value: IRelationshipAttributeHints | RelationshipAttributeHintsJSON
