4e9b734fd8b612f7f03e8f6ffc997c5fa9f92657
Lea Zimmermann <35802341+Lea-Zim@users.noreply.github.com>
2022-05-23 11:37:11 +0200

    Fix/replace validity period in query (#49)
    * fix: adapt validity query
    
    * fix: remove unused abstractPeriod
    
    * chore: bump up content version
    
    * fix: camel case

--- src/attributes/AbstractAttributeQuery.ts
+++ src/attributes/AbstractAttributeQuery.ts
@@ -1,16 +1,17 @@
 import { serialize, validate } from "@js-soft/ts-serval"
-import { CoreSerializable, ICoreSerializable } from "@nmshd/transport"
+import { CoreDate, CoreSerializable, ICoreDate, ICoreSerializable } from "@nmshd/transport"
 import { ContentJSON } from "../ContentJSON"
-import { AbstractPeriod, AbstractPeriodJSON, IAbstractPeriod } from "./types/dates/AbstractPeriod"
 
 export interface AbstractAttributeQueryJSON extends ContentJSON {
-    valueType: string
-    validityPeriod?: AbstractPeriodJSON
+    valueType?: string
+    validFrom?: string
+    validTo?: string
 }
 
 export interface IAbstractAttributeQuery extends ICoreSerializable {
-    valueType: string
-    validityPeriod?: IAbstractPeriod
+    valueType?: string
+    validFrom?: ICoreDate
+    validTo?: ICoreDate
 }
 
 export abstract class AbstractAttributeQuery extends CoreSerializable implements IAbstractAttributeQuery {
@@ -22,5 +23,9 @@ export abstract class AbstractAttributeQuery extends CoreSerializable implements
 
     @serialize()
     @validate({ nullable: true })
-    public validityPeriod?: AbstractPeriod
+    public validFrom?: CoreDate
+
+    @serialize()
+    @validate({ nullable: true })
+    public validTo?: CoreDate
 }
