d744abb0e9f5d55b5c4cce7546983d9854104c15
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2022-06-02 12:12:45 +0200

    Fix/CreateAttributeRequestItem-is-not-exported (#59)
    * fix: export CreateAttributeRequestItem
    
    * chore: set version to 2.0.0-alpha.27

--- src/requests/items/index.ts
+++ src/requests/items/index.ts
@@ -1,2 +1,4 @@
+export * from "./createAttribute/CreateAttributeAcceptResponseItem"
+export * from "./createAttribute/CreateAttributeRequestItem"
 export * from "./readAttribute/ReadAttributeAcceptResponseItem"
 export * from "./readAttribute/ReadAttributeRequestItem"
