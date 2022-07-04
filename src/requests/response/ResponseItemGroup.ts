f03b80746fdce937c98047e1eefd53fab3278f7f
tnotheis <timo.notheisen@js-soft.com>
2022-07-04 15:29:45 +0200

    feat: remove metadata from response classes

--- src/requests/response/ResponseItemGroup.ts
+++ src/requests/response/ResponseItemGroup.ts
@@ -4,12 +4,10 @@ import { IResponseItem, ResponseItem, ResponseItemJSON } from "./ResponseItem"
 
 export interface ResponseItemGroupJSON extends ContentJSON {
     items: ResponseItemJSON[]
-    metadata?: object
 }
 
 export interface IResponseItemGroup extends ISerializable {
     items: IResponseItem[]
-    metadata?: object
 }
 
 @type("ResponseItemGroup")
@@ -17,10 +15,6 @@ export class ResponseItemGroup extends Serializable {
     @serialize()
     @validate({ customValidator: (v) => (v.length < 1 ? "may not be empty" : undefined) })
     public items: ResponseItem[]
-
-    @serialize()
-    @validate({ nullable: true })
-    public metadata?: object
 
     public static from(value: IResponseItemGroup | ResponseItemGroupJSON): ResponseItemGroup {
         return this.fromAny(value)
